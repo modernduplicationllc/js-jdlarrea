74898222b4c9702ad9dfad6f360a6ed49fca1765
jdlarrea <jdlarrea@outlook.com>
2026-08-24 14:08:33 -0400

    scaffolding

--- components/globals/header-main.tsx
+++ components/globals/header-main.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function HeaderMain() {
 	return (
 		<div className="header-main">
-			Header Main
+			<div className="logo">logo goes here</div>
+
+			<div className="nav-items">
+				<Link href="/work">Work</Link>
+				<Link href="/apps">Demo Apps</Link>
+				<Link href="/about">About</Link>
+				<Link href="/resume">Resume</Link>
+			</div>
+
+			<div className="header-links">
+				<Link href="https://github.com">github</Link>
+				<Link href="https://linkedin.com">linkedin</Link>
+			</div>
 		</div>
 	);
 }
