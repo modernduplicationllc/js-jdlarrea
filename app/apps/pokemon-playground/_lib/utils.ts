ac8b3b221ca8be574d3a36d4138e2b6a9d001d92
jdlarrea <jdlarrea@outlook.com>
2026-09-05 13:55:50 -0400

    initial helper function

---
+++ app/apps/pokemon-playground/_lib/utils.ts
@@ -0,0 +1,3 @@
+export function spriteSize( px: number ) {
+	return ((px / 640) * 100).toString();
+}
--- app/apps/pokemon-playground/page.tsx
+++ app/apps/pokemon-playground/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import shrub from "./_assets/terrain/shrub.png";
+import { spriteSize } from "./_lib/utils";
+
 export default function Page() {
 	return (
 		<section className="canvas-container
@@ -5,11 +9,40 @@ export default function Page() {
 		mt-10 p-wrapper-gutter-mobile brm76:p-wrapper-gutter-desktop
 		border-t">
 			<div className="canvas-main
-			w-full max-w-150 h-auto
+			w-full max-w-160 h-auto
 			aspect-square
 			bg-wa-pp-grass
 			relative"
-			></div>
+			>
+				<Image
+					src={shrub}
+					alt=""
+					style={`width: ${spriteSize(16)}%;`}
+					className="h-auto
+					absolute top-[10%] left-[10%]
+					-translate-x-1/2
+					-translate-y-1/2
+					pointer-events-none"
+				/>
+				<Image
+					src={shrub}
+					alt=""
+					className="w-[5%] h-auto
+					absolute top-[15%] left-[10%]
+					-translate-x-1/2
+					-translate-y-1/2
+					pointer-events-none"
+				/>
+				<Image
+					src={shrub}
+					alt=""
+					className="w-[5%] h-auto
+					absolute top-[10%] left-[15%]
+					-translate-x-1/2
+					-translate-y-1/2
+					pointer-events-none"
+				/>
+			</div>
 		</section>
 	)
 }
