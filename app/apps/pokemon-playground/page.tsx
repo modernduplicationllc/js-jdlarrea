6dfd991df0e159e0d7069a524db49f69b298b5d2
jdlarrea <jdlarrea@outlook.com>
2026-09-05 13:29:04 -0400

    style

--- app/apps/pokemon-playground/page.tsx
+++ app/apps/pokemon-playground/page.tsx
@@ -4,7 +4,12 @@ export default function Page() {
 		flex justify-center
 		mt-10 p-wrapper-gutter-mobile brm76:p-wrapper-gutter-desktop
 		border-t">
-			<div className="canvas-main w-full max-w-150 h-auto aspect-square bg-wa-pp-grass"></div>
+			<div className="canvas-main
+			w-full max-w-150 h-auto
+			aspect-square
+			bg-wa-pp-grass
+			relative"
+			></div>
 		</section>
 	)
 }
